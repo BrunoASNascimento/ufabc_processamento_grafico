7b2db56fd7055d59eeeef3673e96ea2e0ce81965
BrunoASNascimento <bruno-asn@outlook.com>
2022-10-04 22:53:12 -0300

    Programa

---
+++ lab_02/GL_TRIANGLES_color.cpp
@@ -0,0 +1,72 @@
+/*
+ * Meu Primeiro Programa: xxx.cpp
+ * <NomeCompleto>, RA <RA>, data DD/MM/AAAA
+ */
+
+#define GL_SILENCE_DEPRECATION
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+#include <GLUT/glut.h>
+#include <unistd.h> // needed to sleep
+
+/* ASCII code for the escape key. */
+#define ESCAPE 27
+
+/* The number of our GLUT window */
+int window;
+
+void display(void)
+{
+    /* clear all pixels  */
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    /* draw white polygon (rectangle) with corners at
+     * (0.25, 0.25, 0.0) and (0.75, 0.75, 0.0)
+     */
+    glColor3f(1.0, 1.0, 1.0);
+    glBegin(GL_TRIANGLES);
+    glColor3f(1.f, 0.f, 0.f);
+    glVertex3f(0.2f, 0.2f, 0.f);
+    glColor3f(0.f, 1.f, 0.f);
+    glVertex3f(0.8f, 0.2f, 0.f);
+    glColor3f(0.f, 0.f, 1.f);
+    glVertex3f(0.2f, 0.8f, 0.f);
+    glEnd();
+
+    /* don't wait!
+     * start processing buffered OpenGL routines
+     */
+    glFlush();
+}
+
+void init(void)
+{
+    /* select clearing color     */
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+
+    /* initialize viewing values  */
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+}
+
+/*
+ * Declare initial window size, position, and display mode
+ * (single buffer and RGBA).  Open window with "hello"
+ * in its title bar.  Call initialization routines.
+ * Register callback function to display graphics.
+ * Enter main loop and process events.
+ */
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(250, 250);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Bruno Nascimento");
+    init();
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0; /* ANSI C requires main to return int. */
+}
